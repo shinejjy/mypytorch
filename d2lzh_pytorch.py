57c8c03bec780700faaddbc0c0a3b324e72a905c
ishine <1263859380@qq.com>
2023-01-07 22:48:55 +0800

    2023.1.7

--- d2lzh_pytorch.py
+++ d2lzh_pytorch.py
@@ -143,11 +143,3 @@ class FlattenLayer(nn.Module):  # 张量扁平化
     def forward(self, x):  # x shape: (batch, *, *, ...)
         return x.view(x.shape[0], -1)
 
-
-def corr2d(X, K):
-    h, w = K.shape
-    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
-    for i in range(Y.shape[0]):
-        for j in range(Y.shape[1]):
-            Y[i, j] = (X[i: i+h, j: j+w] * K).sum()
-    return Y
